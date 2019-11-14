4ee14ed34199592f867a85b6c95bb0dcf1659712
santiagoVVRR <santiagovalencia27@gmail.com>
2019-11-13 20:45:36 -0500

    ..

--- src/model/Index.java
+++ src/model/Index.java
@@ -1,5 +1,6 @@
 package model;
 
+import java.io.File;
 import java.util.ArrayList;
 
 public class Index {
@@ -8,13 +9,25 @@ public class Index {
 	
 	private ArrayList<User> users;
 	private Field upField;
-	private Character headCharacter;
+	private Character upCharacter;
 	private Character characterChoose;
 	private Field fieldChoose;
 	private User userChoose;
 	
 	
-	
+	public index() {
+		File f = new File("files/Users.dat");
+		if(f.exists() == false) {
+			users = new ArrayList<User>();
+		}else {
+			users = recoverUsers();
+		}
+		loadCharacter();
+		loadFiles();
+		circularListOfCharacters();
+		characterChoose = upCharacter;
+		fieldChoose = upField;
+	}
 	
 	
 }
