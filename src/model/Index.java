f910ad033e919883c9d5eb8e3b459127f910a5da
santiagoVVRR <santiagovalencia27@gmail.com>
2019-11-16 11:10:12 -0500

    algunos metodos de ordenamiento en la clase Index

--- src/model/Index.java
+++ src/model/Index.java
@@ -29,7 +29,62 @@ public class Index {
 		fieldChoose = upField;
 	}
 	
+	public void registerUsers(String name) {
+		if(name.length() < 3) {
+			throw new NickNameIsNotValid();
+		}
+		for(int i = 0; i < users.size(); i++) {
+			if(users.get(i).getName().equalsIgnoreCase(name)) {
+				throw new UserIsAlreadyTaken();
+			}
+		}
+		User added = new User(name,0);
+		users.add(added);
+		this.userChoose = added;
+	}
 	
+	public void sortUsersByNameBubble() {
+		for(int i = 0; i < users.size(); i++) {
+			for(int j = users.size()-1; j >= i; j--) {
+				if(users.get(j).compareTo(users.get(j-1)) < 0) {
+					User auxiliar = users.get(j);
+					users.set(j, users.get(j-1));
+					users.set(j-1, auxiliar);
+				}
+			}
+		}
+	}
 	
+	public void sortUsersByBestGameInsertion() {
+		for(int i = 1; i < users.size();i++) {
+			for(int j = users.size()-1; j >= i; j--) {
+				if(users.get(j).getBestGame().compareTo(users.get(j-1).getBestGame()) > 0) {
+					User auxiliar = users.get(j);
+					users.set(j, users.get(j-1));
+					users.set(j-1, auxiliar);
+				}
+			}
+		}
+	}
+	
+	public void sortUsersFistGameInsertion() {
+		for(int i = 1; i<users.size();i++) {
+			for(int j = 1;(j>0) && (users.get(j-1).getFirstGame().compareTo(users.get(j).getFirstGame())> 0); j--){
+				User auxiliar = users.get(j);
+				users.set(j, users.get(j-1));
+				users.set(j-1, auxiliar);
+			}
+		}
+	}
+	
+	public void sortUsersLastGameInsertion() {
+		for(int i = 1; i<users.size();i++) {
+			for(int j = 1;(j>0) && (users.get(j-1).getLastGame().compareTo(users.get(j).getLastGame())> 0); j--){
+				User auxiliar = users.get(j);
+				users.set(j, users.get(j-1));
+				users.set(j-1, auxiliar);
+			}
+		}
+	}
 	
 }
