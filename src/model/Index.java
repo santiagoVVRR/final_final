26c420ebf638652e485e0b5892ae6284a95591ad
santiagoVVRR <santiagovalencia27@gmail.com>
2019-11-16 14:38:59 -0500

    unos cuantos metodos de cargar archivos

--- src/model/Index.java
+++ src/model/Index.java
@@ -1,9 +1,19 @@
 package model;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.sql.Savepoint;
 import java.util.ArrayList;
+import java.util.Comparator;
 
-public class Index {
+public class Index implements Comparator<User>{
 
 	//Attributes
 	
@@ -15,7 +25,7 @@ public class Index {
 	private User userChoose;
 	
 	
-	public index() {
+	public Index() {
 		File f = new File("files/Users.dat");
 		if(f.exists() == false) {
 			users = new ArrayList<User>();
@@ -55,6 +65,22 @@ public class Index {
 		}
 	}
 	
+	public void sortUsersByScore() {
+		for(int i = 0; i < users.size(); i++) {
+			int c = i;
+			User big = users.get(i);
+			for(int j = i+1; j < users.size(); j++) {
+				if(compare(big, users.get(j)) < 0) {
+					big = users.get(j);
+					c = j;
+				}
+			}
+			User temp = users.get(i);
+			users.set(i, big);
+			users.set(c, temp);
+		}
+	}
+	
 	public void sortUsersByBestGameInsertion() {
 		for(int i = 1; i < users.size();i++) {
 			for(int j = users.size()-1; j >= i; j--) {
@@ -85,6 +111,250 @@ public class Index {
 				users.set(j-1, auxiliar);
 			}
 		}
+	}
+	
+	/*
+	 * Method writeUsersNames
+	 * returns a String with all users information
+	 * @return c - list of Users with their names and scores
+	 */
+	public String writeUsersNames() {
+		String c = "";
+		for(int i = 0; i < users.size(); i++) {
+			c += users.get(i).getName() + "\t" + users.get(i).getScore() + ",";
+		}
+		return c;
+	}
+	
+	public String writeUsersNScores() {
+		sortUsersByScore();
+		String c = "";
+		for(int i = 0; i < users.size(); i++) {
+			c += users.get(i).getName() +"\t"+ users.get(i).getScore()+",";
+		}
+		return c;
+	}
+	
+	public String writeUsersBestGame() {
+		sortUsersByBestGameInsertion();
+		String c = "";
+		for(int i = 0; i < users.size(); i++) {
+			c += users.get(i).getName() +"\t"+ users.get(i).getBestGame() +",";
+		}
+		return c;
+	}
+	
+	public String writeUserLastGame() {
+		sortUsersLastGameInsertion();
+		String c = "";
+		for(int i = 0; i < users.size(); i++) {
+			c += users.get(i).getName() +"\t"+ users.get(i).getLastGame()+",";
+		}
+		return c;
+	}
+	
+	public String writeUsersFirstGame() {
+		sortUsersFistGameInsertion();
+		String c = "";
+		for(int i = 0; i < users.size(); i++) {
+			c += users.get(i).getName() + "\t"+ users.get(i).getFirstGame() + ",";
+		}
+		return c;
+	}
+	
+	public void loadCharactersFile() {
+		try {
+			FileReader fr = new FileReader("data/Characters.txt");
+			BufferedReader br = new BufferedReader(fr);
+			String c = "";
+			while((c = br.readLine()) != null) {
+				String[] line = c.split(";");
+				int life = Integer.parseInt(line[0]);
+				int power = Integer.parseInt(line[1]);
+				String name = line[2];
+				String image = line[3];
+				Character c1 = new Character(life,power,name,image);
+				saveCharacter(c1,this.upCharacter,null);
+			}
+			br.close();
+		}catch(FileNotFoundException e) {
+			e.printStackTrace();
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void loadFieldsFile() {
+		try {
+			FileReader fr = new FileReader("data/Fields.txt");
+			BufferedReader br = new BufferedReader(fr);
+			String c = "";
+			while((c = br.readLine()) != null) {
+				String[] line = c.split(";");
+				String name = line[0];
+				String image = line[1];
+				Field f = new Field(name,image);
+				saveField(f,this.upField,null);
+			}
+			br.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void saveFiedlList(Field f,Field current,Field previous) {
+		if(upField != null) {
+			if(upField.getPrev() != null) {
+				notCircularListField();
+			}
+		}
+		if(this.upField == null) {
+			this.upField = f;
+		}else {
+			if(current == null) {
+				f.setPrev(previous);
+				previous.setNext(f);
+			}else {
+				if(this.upField.compareTo(f) <= 0) {
+					f.setNext(upField);
+					upField.setPrev(f);
+					upField = f;
+				}else if(current.compareTo(f) <= 0) {
+					if(previous != null) {
+						previous.setNext(f);
+					}
+					current.setPrev(f);
+					f.setNext(current);
+					f.setPrev(previous);
+				}else {
+					previous = current;
+					current = current.getNext();
+					saveFiedlList(f, current, previous);
+				}
+			}
+		}
+	}
+	
+	public void saveCharactersList(Character c, Character current, Character previous) {
+		if(upCharacter != null) {
+			if(upCharacter.getPrev() != null) {
+				notCircularListField();
+			}
+		}
+		if(this.upCharacter == null) {
+			this.upCharacter = c;
+		}else {
+			if(current == null) {
+				c.setPrev(previous);
+				previous.setNext(c);
+			}else {
+				if(this.upCharacter.compareTo(c) <= 0) {
+					c.setNext(upCharacter);
+					upCharacter.setPrev(c);
+					upCharacter = c;
+				}else if(current.compareTo(c) <= 0) {
+					if(previous != null) {
+						previous.setNext(c);
+					}
+					current.setPrev(c);
+					c.setNext(current);
+					c.setPrev(previous);
+				}else {
+					previous = current;
+					current = current.getNext();
+					saveCharactersList(c, current, previous);
+				}
+			}
+		}
+	}
+	
+	public void charactersCircularList() {
+		Character current = upCharacter;
+		Character previous = null;
+		while(current != null) {
+			previous = current;
+			current = current.getNext();
+		}
+		previous.setNext(upCharacter);
+		upCharacter.setPrev(previous);
+	}
+
+	public void fieldsCircularList() {
+		Field current = upField;
+		Field previous = null;
+		while(current != null) {
+			previous = current;
+			current = current.getNext();
+		}
+		previous.setNext(upField);
+		upField.setPrev(previous);
+	}
+	
+	public Field visualizeNextField(Field current) {
+		return current.getNext();
+	}
+	
+	public Character visualizePrevCharacter(Character current) {
+		return current.getPrev();
+	}
+	
+	
+	
+	public void visualizeNextCharacter() {
+		this.characterChoose = this.characterChoose.getNext();
+	}
+	
+	public void visualizePrevCharacter() {
+		this.characterChoose = this.characterChoose.getPrev();
+	}
+	
+	public void visualizeNextField() {
+		this.fieldChoose = this.fieldChoose.getNext();
+	}
+	
+	public void visualizePrevField() {
+		this.fieldChoose = this.fieldChoose.getPrev();
+	}
+	
+	
+	public void selectedCharacter(boolean s) throws NotChoosenCharacter{
+		if(s == false) {
+			throw new NotChoosenCharacter();
+		}
+	}
+	
+	public void selectedField(boolean s) throws NotChoosenField{
+		if(s == false) {
+			throw new NotChoosenField();
+		}
+	}
+	
+	public void UsersSerialize() throws FileNotFoundException, IOException {
+		File f = new File("data/Users.dat");
+		if(f.exists() == false) {
+			ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(f));
+			oos.writeObject(users);
+			oos.close();	
+		}else {
+			FileWriter fl = new FileWriter(f);
+			BufferedWriter bf = new BufferedWriter(fl);
+			bf.write("");
+			bf.close();
+			ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(f));
+			oos.writeObject(users);
+			oos.close();
+		}
+	}
+	
+	
+	
+	@Override
+	public int compare(User arg0, User arg1) {
+		// TODO Auto-generated method stub
+		return 0;
 	}
 	
 }
